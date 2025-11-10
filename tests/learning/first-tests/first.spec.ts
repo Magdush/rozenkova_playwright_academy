7f71c12201053ead23deb7bcdec173d59b2bd7b6
Magdaléna Roženková <magdalena.rozenkova@netdirect.cz>
2025-11-10 17:27:27 +0100

    Playwright init and create first test, modify actions config to run only 1 test

---
+++ tests/learning/first-tests/first.spec.ts
@@ -0,0 +1,9 @@
+import { test } from "@playwright/test";
+
+test("First Test", async ({ page }) => {
+  await page.goto("https://tredgate.com/pmtool");
+  await page.locator("#username").fill("pw_academy");
+  await page.locator("#password").fill("Playwright321");
+  await page.locator('[type="submit"]').click();
+});
+// ? 'snippet: pwt (pw-test)
